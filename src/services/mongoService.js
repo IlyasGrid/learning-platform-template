342e6df39a2b5b46f6fdc94fa3c445423033feaa
ilyas <ilyas.grid@usmba.ac.ma>
2025-01-13 17:30:55 +0100

    Implement mongo Services

--- src/services/mongoService.js
+++ src/services/mongoService.js
@@ -2,13 +2,32 @@
 // Réponse: 
 
 const { ObjectId } = require('mongodb');
+const db = require('../config/db');
 
 // Fonctions utilitaires pour MongoDB
-async function findOneById(collection, id) {
-  // TODO: Implémenter une fonction générique de recherche par ID
+async function createCourse(course) {
+  const collection = db.db.collection('courses');
+  const result = await collection.insertOne(course);
+  return result.ops[0];
+}
+
+async function getCourse(id) {
+  const collection = db.db.collection('courses');
+  const course = await collection.findOne({ _id: ObjectId(id) });
+  return course;
+}
+
+async function getCourseStats() {
+  const collection = db.db.collection('courses');
+  const stats = await collection.aggregate([
+    { $group: { _id: null, totalCourses: { $sum: 1 } } }
+  ]).toArray();
+  return stats[0];
 }
 
 // Export des services
 module.exports = {
-  // TODO: Exporter les fonctions utilitaires
+  createCourse,
+  getCourse,
+  getCourseStats,
 };
